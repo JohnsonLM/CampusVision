9e29126bb53c69b714802d76bc3f62afa0eda407
Johnson Martin <johnmartin31899@gmail.com>
2022-06-05 19:15:17 -0400

    Removed issues

--- app/static/js/weather.js
+++ app/static/js/weather.js
@@ -4,8 +4,6 @@ function weatherBalloon() {
   .then(function(data) {
     drawWeather(data.daily);
   })
-  .catch(function() {
-  });
 }
 
 function drawWeather(d) {
@@ -13,12 +11,10 @@ function drawWeather(d) {
       var celcius = Math.round(parseFloat(d[day].temp.day)-273.15);
       var fahrenheit = Math.round(((parseFloat(d[day].temp.day)-273.15)*1.8)+32);
       var dt_txt = new Date(d[day].dt*1000)
-      day_plus = day + 1
+      var day_plus = day + 1
       var element = 'day' + day_plus
       document.getElementById(element).innerHTML =
-      '<h3>' + dt_txt.toLocaleDateString('en-us', {day:"numeric", weekday:"short", month:"short"}) + '</h3>' +
-      '<img src="http://openweathermap.org/img/wn/' + d[day].weather[0].icon + '@2x.png">' +
-      '<h4>' + fahrenheit + '&deg; f' + '</h4>'
+      '<h3>${dt_txt.toLocaleDateString('en-us', {day:"numeric", weekday:"short", month:"short"})}</h3><img src="http://openweathermap.org/img/wn/${d[day].weather[0].icon}@2x.png"><h4> ${fahrenheit}&deg; f</h4>'
     }
 }
 
